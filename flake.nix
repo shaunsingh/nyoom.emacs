4e4e77ad7f52a96dcfc3e5e5a4737ea1caf17e83
shaunsingh <shaunsingh0207@gmail.com>
2021-12-07 10:15:08 -0500

    first commit

---
+++ flake.nix
@@ -0,0 +1,29 @@
+
+{
+  description = "Sharya Singh's Doom-emacs config";
+
+  nixConfig.extra-substituters = "https://nix-community.cachix.org";
+  nixConfig.extra-trusted-public-keys = "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    emacs.url = "github:nix-community/emacs-overlay";
+    doom-emacs.url = "github:hlissner/doom-emacs/develop";
+    doom-emacs.flake = false;
+  };
+
+  outputs = { self, nixpkgs, flake-utils, emacs, doom-emacs }:
+    flake-utils.lib.simpleFlake {
+      inherit self nixpkgs;
+      name = "doom-emacs";
+      preOverlays = [
+        emacs.overlay
+        (final: prev: { doomEmacsRevision = doom-emacs.rev; })
+      ];
+      shell = ./shell.nix;
+      systems = [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" ];
+    };
+}
+
+
---
+++ shell.nix
@@ -0,0 +1,49 @@
+
+{ pkgs ? import <nixpkgs> {
+  overlays = [
+    (import (builtins.fetchTarball {
+      url =
+        "https://github.com/nix-community/emacs-overlay/archive/master.tar.gz";
+    }))
+  ];
+} }:
+with pkgs;
+mkShell {
+  buildInputs = [ 
+     # Emacs deps
+     ((emacsPackagesNgGen emacsGcc).emacsWithPackages
+       (epkgs: [ epkgs.vterm ]))
+     (ripgrep.override { withPCRE2 = true; })
+     sqlite
+     gnuplot
+     # pandoc
+     # sdcv
+     (aspellWithDicts (ds: with ds; [ en en-computers en-science ]))
+     (texlive.combine {
+       inherit (texlive)
+         scheme-small dvipng dvisvgm l3packages xcolor soul adjustbox
+         collectbox amsmath siunitx cancel mathalpha capt-of chemfig
+         wrapfig mhchem fvextra cleveref latexmk tcolorbox environ arev
+         amsfonts simplekv alegreya sourcecodepro newpx svg catchfile
+         transparent hanging biblatex biblatex-mla;
+     })
+     # languagetool
+     nixfmt
+     fd
+     # sassc
+  ];
+  shellHook = ''    
+    export DOOMDIR="$(pwd)"
+    export DOOMLOCALDIR="$(pwd)"
+    if [ ! -d $HOME/.config/emacs/.git ]; then
+      mkdir -p $HOME/.config/emacs
+      git -C $HOME/.config/emacs init
+    fi
+    if [ $(git -C $HOME/.config/emacs rev-parse HEAD) != ${pkgs.doomEmacsRevision} ]; then
+      git -C $HOME/.config/emacs fetch https://github.com/hlissner/doom-emacs.git || true
+      git -C $HOME/.config/emacs checkout ${pkgs.doomEmacsRevision} || true
+      $HOME/.config/emacs/bin/doom sync || true
+      YES=1 FORCE=1 $HOME/.config/emacs/bin/doom sync -u &
+    fi
+  '';
+}
